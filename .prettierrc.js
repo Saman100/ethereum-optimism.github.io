4b16b1a9ac23d931ee49ed7a3ed573bba76fba46
Saman100 <147325838+Saman100@users.noreply.github.com>
2024-02-06 14:27:39 -0800

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   $schema: 'http://json.schemastore.org/prettierrc',
   trailingComma: 'es5',
   tabWidth: 2,
-  semi: false,
+  semi: true,
   singleQuote: true,
   arrowParens: 'always',
 }
